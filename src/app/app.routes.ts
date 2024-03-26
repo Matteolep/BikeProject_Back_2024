722d3e0d56220e7c584b09116c03a3a985442c0f
Matteolep <lepagnolmatteo@gmail.com>
2024-03-26 09:58:34 +0100

    v1

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import {LightPageComponent} from "./components/light-page/light-page.component";
-import {RacinePageComponent} from "./components/racine-page/racine-page.component";
-
-export const routes: Routes = [
-  {path: "racine", component: RacinePageComponent},
-  {path: "light", component: LightPageComponent},
-  {path: "**", redirectTo: "light"}
-];
